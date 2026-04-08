f595f5160021404270b4e31756791f976a55cdb2
Littlemosa <amosr834@gmail.com>
2026-04-08 14:48:19 +0300

    change the name of the front end folder to Front-end

---
+++ Front-end/src/App.jsx
@@ -0,0 +1,22 @@
+import Login from "./pages/Login";
+
+function Header() {
+  return (
+    <div>
+      <h2 style={{ padding: "20px" }}>
+        Welcome to Elimu School where we nurture young minds and empower future leaders. Explore our dashboard
+      </h2>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Login />
+    </div>
+  );
+}
+
+export default App;

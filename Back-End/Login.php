867eb637f961fbc1b0aa7a8813221f3b731aa207
Elvis-were <165947172+Elvis-were@users.noreply.github.com>
2026-04-17 12:06:56 +0300

    Added the login.php file

--- Back-End/Login.php
+++ Back-End/Login.php
@@ -1,14 +1,8 @@
 <?php
+require_once("Cors.php");
 header("content-Type: application/json");
-include 'elimumanagementsystem.sql';
-$adm_no;
-$userPassword;
-
-if(isset($adm_no) && isset($userPassword)){
-
-}
-else{
-
-}
-
 ?>
+
+
+
+

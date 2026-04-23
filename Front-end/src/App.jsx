d69ed8b725f4370d436fd563bbb4af0ece47d11d
Elvis-were <165947172+Elvis-were@users.noreply.github.com>
2026-04-23 13:40:11 +0300

    Added the browser router

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -4,12 +4,28 @@ import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Profile from "./pages/Profile";
 import Academic from "./pages/Academic";
+import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Academic />
-    </>
+    <BrowserRouter>
+    //navigation 
+    <nav>
+    <Link to="/">Login</Link>
+    <Link to="/password">Password</Link>
+    <Link to="/">Login</Link>
+    <Link to="/">Login</Link>
+    </nav>
+    //setting up the routes
+    <Routes>
+    <Route path="/" element={Login}/>
+    <Route path="/password" element={PasswordReset}/>
+    //new routes will be added
+    </Routes>
+
+    </BrowserRouter>
+      
+    
   );
 }
 

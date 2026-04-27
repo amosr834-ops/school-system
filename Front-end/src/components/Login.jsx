b733d48ade042bcb7a4a6ecef34b86aab4711287
Elvis-were <165947172+Elvis-were@users.noreply.github.com>
2026-04-27 16:27:51 +0300

    Build the system for production

--- Front-end/src/components/Login.jsx
+++ Front-end/src/components/Login.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
-const logo = "/Logo.png";
 const backgroundImage = "/background.png";
 
 function Login() {
@@ -61,7 +60,7 @@ function Login() {
       <div className="left-panel">
         
         <h1>ELIMU SCHOOL</h1>
-        <img src={background} alt="Elimu School Logo" />
+        <img src={backgroundImage} alt="Elimu School Logo" />
       
       </div>
 
